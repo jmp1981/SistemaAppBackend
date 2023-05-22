298a00f4ef90e356632e137facd842e83168c3e3
jmp1981 <martinperalta1981@gmail.com>
2023-05-21 22:06:47 -0300

    commit inicial

---
+++ SisAppBackend/src/main/java/com/sisapp/prepro/SisAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sisapp.prepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisAppBackendApplication.class, args);
+	}
+
+}
---
+++ SisAppBackend/src/test/java/com/sisapp/prepro/SisAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisapp.prepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
